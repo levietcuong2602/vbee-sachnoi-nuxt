6457011b7dab37e26a4f795625164ed5448641d3
cuonglv <levietcuong0297@gmail.com>
2019-10-06 22:30:55 +0700

    update combine api get list books

--- store/getters.js
+++ store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   token: state => state.user.token,
   avatar: state => state.user.avatar,
   name: state => state.user.name,
+  userId: state => state.user.userId,
   breadScrum: state => state.breadScrum
 };
 export default getters;
