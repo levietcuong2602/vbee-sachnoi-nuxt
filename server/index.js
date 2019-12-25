e2fac5dff87de7f46553867b45cb42a7e22f89b3
minhqp <phamquangminh.hust@gmail.com>
2019-12-25 17:19:25 +0700

    fix env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+require('dotenv').config();
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
@@ -10,8 +11,6 @@ config.dev = process.env.NODE_ENV !== 'production'
 async function start () {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
-
-  const { host, port } = nuxt.options.server
 
   // Build only in dev mode
   if (config.dev) {
@@ -25,9 +24,11 @@ async function start () {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  const port = process.env.PORT;
+  const host = 'localhost';
+  app.listen(port, host);
   consola.ready({
-    message: `Server listening on http://${host}:${port}`,
+    message: `Server is listening on ${port}:${port}`,
     badge: true
   })
 }
